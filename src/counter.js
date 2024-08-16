77e08d45fc165056420dd1b9c05c23f97b0a046c
svaddi <svaddi@paycor.com>
2024-08-16 09:07:55 -0500

    directives

--- src/counter.js
+++ src/counter.js
@@ -1,12 +1,14 @@
 import styles from './counter.module.css';
 import './lessy.less';
-console.log(stylesfoo);
+import steveImage from './steve-after-a-workshop.jpg?h=400&format=webp&as=metadata';
+
 export const initializeCounter = (doc = globalThis.document) => {
   const countElement = doc.getElementById('count');
 
   countElement.classList.add(styles.count);
   const incrementButton = doc.getElementById('increment');
   const decrementButton = doc.getElementById('decrement');
+  const content = document.querySelector('#content');
 
   let count = 0;
 
@@ -16,12 +18,14 @@ export const initializeCounter = (doc = globalThis.document) => {
       import('./banner').then(({ displayBanner }) => {
         displayBanner();
       });
+    } else {
+      import('./banner').then(({ hideBanner }) => {
+        hideBanner();
+      });
     }
-    // else {
-    //   import('./banner').then(({hideBanner }) => {
-    //    hideBanner()
-    //  });
-    // }
+    const imageElement = document.createElement('img');
+    imageElement.src = steveImage.src;
+    content.appendChild(imageElement);
   };
 
   const increment = () => {
